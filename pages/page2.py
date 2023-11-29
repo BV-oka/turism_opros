ddc2911cf459629bdc742a68721efed479b579e9
BV-oka <138525513+BV-oka@users.noreply.github.com>
2023-11-29 16:46:28 +0300

    Update page2.py

--- pages/page2.py
+++ pages/page2.py
@@ -1,4 +1,9 @@
 import streamlit as st
+import pandas as pd
 
 st.markdown("# Page 2 ❄️")
 st.sidebar.markdown("# Page 2 ❄️")
+
+df = pd.read_csv("main/datasets/Opros_po_razvitiiu_turizma.csv")
+
+st.dataframe(df)
