fcbad25857be07fa57d1bf0f8579dca932ca6724
BV-oka <138525513+BV-oka@users.noreply.github.com>
2023-11-29 16:37:19 +0300

    Update main.py

--- main.py
+++ main.py
@@ -9,74 +9,75 @@ st.markdown("# Туризм муниципальное образование г
 st.sidebar.markdown("# главная page 🎈")
 
 # Название
-st.title("что-то")
+st.sidebar.title("что-то")
 
 # Заголовок
-st.header("муниципальное образование город Алексин")
+st.sidebar.header("муниципальное образование город Алексин")
 
 # Подзаголовок
-st.subheader("визуализация результатов опроса по развитию туризма")
+st.sidebar.subheader("визуализация результатов опроса по развитию туризма")
 
 # Текст
 st.sidebar.text("Просто текст")
 
 st.sidebar.success("Успех")
-st.info("Information")
-st.warning("Warning")
-st.error("Error")
+st.sidebar.info("Information")
+st.sidebar.warning("Warning")
+st.sidebar.error("Error")
 # st.snow()
 # st.balloons()
 
 
 # полоска
-st.divider()
+st.sidebar.divider()
 
 # переключатель пола
 gender = st.sidebar.radio("выберите пол респондентов: ", ('муж', 'жен'), horizontal=True)
 
 # слайдер возраста
-age = st.slider('Укажите возраст респондентов', 0, 100, 35)
+age = st.sidebar.slider('Укажите возраст респондентов', 0, 100, 35)
 
 # ещё полоска
-st.divider()
+st.sidebar.divider()
 
 
 if gender == 'муж':
-    st.write("вы выбрали респондентов мужчин в возрасте", age)
+    st.sidebar.write("вы выбрали респондентов мужчин в возрасте", age)
 else:
-    st.write("вы выбрали респондентов женщин в возрасте", age)
+    st.sidebar.write("вы выбрали респондентов женщин в возрасте", age)
     
 # ещё полоска
-st.divider()
+st.sidebar.divider()
 
-if st.button('я кнопка -  Нажми на меня'):
-  st.write('надпись - результат нажатия на кнопку!')
+if st.sidebar.button('я кнопка -  Нажми на меня'):
+  st.sidebar.write('надпись - результат нажатия на кнопку!')
 
-genre = st.radio(
+genre = st.sidebar.radio(
     "What's your favorite movie genre",
     [":rainbow[Comedy]", "***Drama***", "Documentary :movie_camera:"],
     captions = ["Laugh out loud.", "Get the popcorn.", "Never stop learning."])
 
 if genre == ':rainbow[Comedy]':
-    st.success('You selected comedy.')
+    st.sidebar.success('You selected comedy.')
 else:
-    st.write("You didn\'t select comedy.")
+    st.sidebar.write("You didn\'t select comedy.")
 
 
-option = st.selectbox(
+option = st.sidebar.selectbox(
     'How would you like to be contacted?',
     ('Email', 'Home phone', 'Mobile phone'))
 
-st.write('You selected:', option)
+st.sidebar.write('You selected:', option)
 
 
 # слайдер интервал
 
-values = st.slider(
+values = st.sidebar.slider(
     'Select a range of values',
     0.0, 100.0, (25.0, 75.0))
-st.write('Values:', values)
+st.sidebar.write('Values:', values)
 
 df = pd.read_csv("datasets/Opros_po_razvitiiu_turizma.csv")
+
 st.dataframe(df)
 
