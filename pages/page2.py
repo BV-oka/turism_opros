473f2d0f6c579f5c1cb4ae85a557f530c98dda9e
BV-oka <138525513+BV-oka@users.noreply.github.com>
2023-11-29 17:08:02 +0300

    Update page2.py

--- pages/page2.py
+++ pages/page2.py
@@ -5,6 +5,6 @@ import pandas as pd
 st.markdown("# Page 2 ❄️")
 st.sidebar.markdown("# Page 2 ❄️")
 
-df = pd.read_csv("../datasets/Opros_po_razvitiiu_turizma.csv")
+df = pd.read_csv("..\datasets/Opros_po_razvitiiu_turizma.csv")
 
 st.dataframe(df)
