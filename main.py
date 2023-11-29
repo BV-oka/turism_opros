e878d7cfbde02edbd0c51e5789b32fb55f2e3122
BV-oka <138525513+BV-oka@users.noreply.github.com>
2023-11-29 14:33:36 +0300

    Update main.py

--- main.py
+++ main.py
@@ -76,3 +76,8 @@ values = st.slider(
     0.0, 100.0, (25.0, 75.0))
 st.write('Values:', values)
 
+url = 'https://github.com/lukes/ISO-3166-Countries-with-Regional-Codes/blob/master/all/all.csv'
+df = pd.read_csv(url,index_col=0)
+#df = pd.read_csv(url)
+
+print(df.head(5))
