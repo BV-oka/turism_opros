8067bccbdc63b7f887d24d7c6ef13da620ef1dc3
BV-oka <138525513+BV-oka@users.noreply.github.com>
2023-11-30 11:54:11 +0300

    Update main.py

--- main.py
+++ main.py
@@ -43,6 +43,19 @@ age = st.sidebar.radio("укажите возраст респондентов: 
 # ещё полоска
 st.sidebar.divider()
 
+# переключатель отношения
+att = st.sidebar.radio("укажите отношение к туристам респондентов: ", ('все','положительное', 'отрицательное'), horizontal=False)
+
+# ещё полоска
+st.sidebar.divider()
+
+col1, col2, col3 = st.columns(3)
+col1.metric(label="Temperature", value="70 °F", delta="1.2 °F")
+col2.metric("Wind", "9 mph", "-8%")
+col3.metric("Humidity", "86%", "4%")
+
+
+
 if gender == 'муж':
     st.sidebar.write("вы выбрали респондентов мужчин в возрасте", age)
 else:
