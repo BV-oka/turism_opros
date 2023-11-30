ad6ddb008f4d7e6bb413419f7043c5ef7e6ee552
BV-oka <138525513+BV-oka@users.noreply.github.com>
2023-11-30 15:05:39 +0300

    Update main.py

--- main.py
+++ main.py
@@ -62,7 +62,8 @@ df = pd.read_csv("datasets/Opros_po_razvitiiu_turizma.csv")
 
 
 # график
-chart_data = pd.DataFrame(df, columns=["a", "b", "c"])
+# chart_data = pd.DataFrame(df, columns=["a", "b", "c"])
+chart_data = pd.DataFrame(df)
 
 st.bar_chart(chart_data)
 
