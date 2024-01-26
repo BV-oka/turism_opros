c4d4152c3325945961110fa66c6f6ea38d29044e
BV-oka <138525513+BV-oka@users.noreply.github.com>
2024-01-26 12:52:17 +0300

    Update main.py

--- main.py
+++ main.py
@@ -76,6 +76,13 @@ def sidebar_input_features():
 # st.write('## Ваши данные')
 # df
 
+options = st.multiselect(
+    'What are your favorite colors',
+    ['Green', 'Yellow', 'Red', 'Blue'],
+    ['Yellow', 'Red'])
+
+st.write('You selected:', options)
+
 # конец оформления боковой панели
 
 # ----- ПАРАМЕТРЫ РЕСПОНДЕНТОВ -----
