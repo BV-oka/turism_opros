3be1d393ad4cafaa7f269c8b74640d5f2de905ce
BV-oka <138525513+BV-oka@users.noreply.github.com>
2024-01-29 17:50:31 +0300

    Update main.py

--- main.py
+++ main.py
@@ -67,11 +67,3 @@ def sidebar_input_features():
     return df
 
 process_main_page()
-
-options = st.multiselect(
-    'What are your favorite colors',
-    ['Green', 'Yellow', 'Red', 'Blue'],
-    ['Yellow', 'Red'])
-
-st.write('You selected:', options)
-
