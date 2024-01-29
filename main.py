e205ba092502c18c6701f38e9b0d50b3f86ffbf3
BV-oka <138525513+BV-oka@users.noreply.github.com>
2024-01-29 14:32:12 +0300

    Update main.py

--- main.py
+++ main.py
@@ -16,6 +16,12 @@ def show_main_page():
     layout="wide"
 )
 
+def sidebar_input_features():
+    gender = st.sidebar.multiselect("выберите пол респондентов: ", ["мужской","женский"], placeholder="пол респондентов")
+    age = st.sidebar.multiselect("укажите возраст респондентов: ", ["до 35 лет", "36-45 лет","46-55 лет", "старше 55 лет"], placeholder="возраст респондентов")
+    att = st.sidebar.multiselect("укажите отношение респондентов к туристам: ", ["положительное", "отрицательное"], placeholder="выбор отношение")
+
+
 if __name__=="__main__":
     process_main_page()
 
