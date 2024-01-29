d2643ac7d9c61b2b24988fa0b0a4640fd9413347
BV-oka <138525513+BV-oka@users.noreply.github.com>
2024-01-29 16:00:13 +0300

    Update main.py

--- main.py
+++ main.py
@@ -60,3 +60,17 @@ def sidebar_input_features():
 
 process_main_page()
 
+options = st.multiselect(
+    'What are your favorite colors',
+    ['Green', 'Yellow', 'Red', 'Blue'],
+    ['Yellow', 'Red'])
+
+st.write('You selected:', options)
+
+options
+
+df = pd.DataFrame(options)
+df
+dfnew = list(df[0])
+dfnew
+
