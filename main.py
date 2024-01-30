d5a2225cb255fdcc151b866291b2badd847f9a82
BV-oka <138525513+BV-oka@users.noreply.github.com>
2024-01-30 16:32:41 +0300

    Update main.py

--- main.py
+++ main.py
@@ -69,7 +69,7 @@ process_main_page()
 st.divider()
 gggg = pd.DataFrame(st.multiselect("выберите: ", ["первое","второе"], placeholder="тест", help="выберите показатели"))
 gggg
-gggg.values()
+gggg.values[0]
 
 # df = pd.DataFrame(gggg)
 # df
